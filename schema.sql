417b3d39e573ad4ab48ee691274a7487b90a8058
Booze33 <tebetisloh@gmail.com>
2023-06-07 17:58:43 -0500

    Add Tables

--- schema.sql
+++ schema.sql
@@ -8,7 +8,30 @@ CREATE TABLE animals (
   escape_attempts INT,
   neutered BOOLEAN,
   weight_kg DECIMAL,
-  PRIMARY KEY (id)
+  PRIMARY KEY(id)
 );
 
 ALTER TABLE animals ADD species VARCHAR(40);
+
+DROP TABLE IF EXISTS owners;
+DROP TABLE IF EXISTS species;
+
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(25) NOT NULL,
+  age INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(25) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT FK_AnimalSpecies FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE ANIMALS ADD CONSTRAINT FK_Owners FOREIGN KEY (owner_id) REFERENCES owners(id);
